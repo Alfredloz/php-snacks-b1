476a01007209bee7cb4f49ff8f3b6cf86dccbd8a
Alfredloz <alfredogregorio.lozano@gmail.com>
2021-01-31 16:41:06 +0100

    secono esericizio iniziato

--- ex2/index2.php
+++ ex2/index2.php
@@ -5,7 +5,14 @@ conosciamo nella documentazione) che:
 3. age sia un numero.
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 <!-- php -->
+<?php 
+    $name = $_GET["name"];
+    $mail = $_GET["mail"];
+    $age = ["age"];
+    $at = strpos($mail, "@");
+    $dot = strpos($mail. ".");
 
+ ?>
 <!-- /php -->
 <!DOCTYPE html>
 <html lang="en">
@@ -15,6 +22,23 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
     <title>Document</title>
 </head>
 <body>
-    
+   <h1>Ciao! questi sono i dati inseriti:</h1>
+   <p>nome:</p>
+   <p><?php 
+    if (strlen($name) < 3 ) {
+        echo 'Nome troppo corto!';
+    } else{
+        echo $name;
+    };
+   ?>
+   </p>
+   <p>
+   <?php if (strpos($mail, '@') === false && strpos($mail, '.') === false) {
+       echo 'Non è una mail valida!';
+   }else{
+       echo $mail;
+   };
+    ?>
+    </p>
 </body>
 </html>
